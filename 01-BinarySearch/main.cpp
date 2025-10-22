fe0f5b8d1359b1c66989360ab74e128be04747b5
Abhinav <abhinavbhushan6@gmail.com>
2025-10-22 19:09:18 +0530

    feat: Archive solution for Ropes (Binary Search on Answer)

--- 01-BinarySearch/main.cpp
+++ 01-BinarySearch/main.cpp
@@ -1,30 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
-int fast_binary_search(int r,const vector<int>& v) {
-    int lo = 0;
-    int hi = v.size()-1;
-    int ans = 0;
-    while (lo <= hi) {
-        int mid = lo + (hi-lo)/2;
-        if (v[mid]<=r) {
-            ans = mid + 1;
-            lo = mid + 1;
+bool isPossible(double length_to_check,const vector<int>& all_ropes,int k_needed) {
+    int total_pieces = 0;
+    // "For each rope_length in the all_ropes vector..."
+    if (length_to_check < 1e-7) {
+        return true;
+    }
+    for (const int rope_length : all_ropes) {
+        // Use rope_length directly
+        int pieces_from_this_rope = static_cast<int>(rope_length / length_to_check);
+        total_pieces = total_pieces + pieces_from_this_rope;
+    }
+    return total_pieces >= k_needed;
+}
+
+double ropes_search(const vector<int>& v , int k, int n) {
+    double lo = 0.0;
+    double ans = 0.0;
+    double hi = 1e7;
+    for (int i = 0; i< 100;i++) { // 100 Iterations are enough for any 10^7
+        double mid = lo + (hi-lo)/2;
+        if (isPossible(mid,v,k)) { // Important check function
+            ans = mid;
+            lo = mid;
         }
         else {
-            hi = mid - 1;
+            hi = mid;
         }
     }
     return ans;
-
 }
-
 int main() {
     ios_base::sync_with_stdio(false);//Important I/O Methods
     cin.tie(NULL);
-    int n;
-    cin >> n;
+    int n,k;
+    cin >> n >> k;
     vector<int> v;
     for (int i = 0; i<n;i++) {
         int value;
@@ -32,15 +45,7 @@ int main() {
         v.push_back(value);
     }
     sort(v.begin(),v.end());
-    int k;
-    cin >>k;
-    for (int i =0;i<k;i++) {
-        int l,r;
-        cin >> l >> r;
-        int count;
-        count = fast_binary_search(r,v) - fast_binary_search(l-1,v);
-        cout << count << endl;
-    }
+    cout << setprecision(20) << ropes_search(v,k,n);
 
     return 0;
 };
